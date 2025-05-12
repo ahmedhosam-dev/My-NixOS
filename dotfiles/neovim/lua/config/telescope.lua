7e8ee004af5916fe04baeff82b2ed15b271ad0b6
ahmedhosam-dev <ahmed0114702@gmail.com>
2025-05-12 14:56:44 +0300

    updated

--- dotfiles/neovim/lua/config/telescope.lua
+++
@@ -1,6 +0,0 @@
--- Enable telescope fuzzy finder
-require('telescope').setup{}
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>')
-vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>')
